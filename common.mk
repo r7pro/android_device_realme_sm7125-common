e4bb975877fcbf61c6c35a2b720da4d66442e750
shaenix <theshaenix@gmail.com>
2025-07-28 21:27:30 +0530

    sm7125-common:removed deprecated bluetooth libs

--- common.mk
+++ common.mk
@@ -77,8 +77,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
-    android.hardware.bluetooth.a2dp@1.0 \
-    android.hardware.bluetooth.a2dp@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
