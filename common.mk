2fb9032b7a0d5e3836c7b76b9a7b661d1f51ff9f
shaenix <theshaenix@gmail.com>
2025-07-28 17:58:48 +0530

    sm7125-common: Enable Java optimizations for SystemUI and System_server

--- common.mk
+++ common.mk
@@ -147,6 +147,10 @@ PRODUCT_PACKAGES += \
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Enable whole-program R8 Java optimizations for SystemUI and system_server
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
