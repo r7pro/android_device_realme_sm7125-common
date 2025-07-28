b255479212d2d5ec3098efaf5ce791b1df9115cf
shaenix <theshaenix@gmail.com>
2025-07-28 19:05:56 +0530

    sm7125-common: Move to new RFS install_symlink targets

--- common.mk
+++ common.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BOARD_PLATFORM := atoll
