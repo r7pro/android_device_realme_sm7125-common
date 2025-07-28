e63c18308ee4e7e76bbf49d286dac28f3fa87c3a
shaenix <theshaenix@gmail.com>
2025-07-28 17:06:34 +0530

    sm7125-common: shift to the lineage

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,7 +187,7 @@ BOARD_USES_METADATA_PARTITION := true
 
 BOARD_FLASH_BLOCK_SIZE := 262144
 
--include vendor/derp/config/BoardConfigReservedSize.mk
+-include vendor/lineage/config/BoardConfigReservedSize.mk
 
 # Platform
 TARGET_BOARD_PLATFORM := atoll
