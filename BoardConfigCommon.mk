dbd4a1f1ddb1aef4e3494e1135591a1612a3b16d
shaenix <theshaenix@gmail.com>
2025-07-28 17:36:12 +0530

    sm7125-common: add qcom-caf compatibility matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,6 +103,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/manifest.xml
 
+DEVICE_MATRIX_FILE := \
+    hardware/qcom-caf/common/compatibility_matrix.xml
+
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
 
